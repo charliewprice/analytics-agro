f2020e4eb3e2be457059374e1111d9960eb18a39
Charlie Price <cprice9739@carolina.rr.com>
2020-05-19 22:48:58 -0400

    update

--- push.sh
+++ push.sh
@@ -3,7 +3,10 @@
 find . -name '*-checkpoint.ipynb' -delete
 find . -name '*.html' -delete
 find . -name '*.pyc' -delete
+rm -rf PorterFarms/EnvMonitoring/.ipynb_checkpoints
+rm -rf PorterFarms/InfMonitoring/.ipynb_checkpoints
 rm -rf PorterFarms/.ipynb_checkpoints
+
 git status
 git add .
 git commit -m "update"
